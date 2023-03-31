7c40b5d3abf26ccf27badd169144ee8c6fd66834
Leyla Shafiyeva <llshafi03@gmail.com>
2023-03-31 20:05:24 +0400

    edit

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,14 +1,20 @@
 ﻿using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+
 namespace Client;
 
 public partial class MainWindow : Window
 {
+    private bool xTurn = true;
+    private int[,] board = new int[3, 3];
+    private TcpClient _tcpClient;
+    Button[,] buttons;
     private readonly string _character;
-    private bool _isTurn;
 
     TcpClient tcpClient;
     BinaryWriter bw;
@@ -17,6 +23,12 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        buttons = new Button[3, 3] {
+        { btn00, btn01, btn02 },
+        {btn00, btn01, btn12},
+        { btn00, btn01, btn22 }
+    };
+
 
         tcpClient = new();
         var ip = IPAddress.Parse("127.0.0.1");
@@ -29,18 +41,170 @@ public partial class MainWindow : Window
 
         var player = br.Read();
 
-        if(player==0)
+        Title="Player "+player.ToString();
+
+        if (player%2==0)
             _character = "X";
         else _character = "O";
-        _isTurn = false;
-       
-    }
 
+        Task.Run(ReceiveUpdates);
+    }
     private void Button_Click(object sender, RoutedEventArgs e)
     {
-        if(sender is Button b)
+        Button button = (Button)sender;
+        int row = Grid.GetRow(button);
+        int column = Grid.GetColumn(button);
+
+        if (xTurn)
         {
-            b.Content = _character;
+            board[row, column] = 1;
+            button.Content = "X";
+        }
+        else
+        {
+            board[row, column] = 2;
+            button.Content = "O";
+        }
+
+        button.IsEnabled = false;
+        xTurn = !xTurn;
+
+        CheckForWinner();
+        string gameState = GetGame();
+        byte[] data = Encoding.ASCII.GetBytes(gameState);
+        bw.Write(data, 0, data.Length);
+    }
+    private Task ReceiveUpdates()
+    {
+        while (true)
+        {
+            byte[] data = new byte[1024];
+            int bytesRead = br.Read(data, 0, data.Length);
+
+            if (bytesRead > 0)
+            {
+                string receivedData = Encoding.ASCII.GetString(data, 0, bytesRead);
+                UpdateGame(receivedData);
+            }
+        }
+    }
+
+    private string GetGame()
+    {
+        string gameState = "";
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                gameState += board[i, j];
+            }
+        }
+
+        return gameState;
+    }
+
+    private void UpdateGame(string gameState)
+    {
+        int index = 0;
+
+        foreach (Button button in buttons)
+        {
+            int value = int.Parse(gameState[index].ToString());
+            board[index / 3, index % 3] = value;
+
+            button.Content = value == 1 ? "X" : value == 2 ? "O" : "";
+            button.IsEnabled = value == 0;
+
+            index++;
+        }
+
+        xTurn = !xTurn;
+    }
+
+    private void CheckForWinner()
+    {
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (board[i, 0] != 0 && board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+            {
+                ShowWinner(board[i, 0]);
+                return;
+            }
+        }
+
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (board[0, i] != 0 && board[0, i] == board[1, i] && board[1, i] == board[2, i])
+            {
+                ShowWinner(board[0, i]);
+                return;
+            }
+        }
+
+
+        if (board[0, 0] != 0 && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+        {
+            ShowWinner(board[0, 0]);
+            return;
+        }
+
+        if (board[0, 2] != 0 && board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+        {
+            ShowWinner(board[0, 2]);
+            return;
+        }
+
+        bool isTie = true;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (board[i, j] == 0)
+                {
+                    isTie = false;
+                    break;
+                }
+            }
+
+            if (!isTie)
+            {
+                break;
+            }
+        }
+
+        if (isTie)
+        {
+            ShowTie();
+        }
+    }
+
+    private void ShowWinner(int player)
+    {
+        MessageBox.Show($"Player {(player == 1 ? "X" : "O")} wins!");
+        ResetGame();
+    }
+
+    private void ShowTie()
+    {
+        MessageBox.Show("It's a tie!");
+        ResetGame();
+    }
+
+    private void ResetGame()
+    {
+        board = new int[3, 3];
+        xTurn = true;
+
+        foreach (var element in Grid.Children)
+        {
+            if (element is Button button)
+            {
+                button.Content = "";
+                button.IsEnabled = true;
+            }
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,24 +1,166 @@
-﻿using System.Net.Sockets;
+﻿using System.IO;
 using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 int playerId = 0;
-var ip = IPAddress.Parse("127.0.0.1");
-var port = 27001;
+IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+int port = 27001;
+char[,] board = new char[3, 3];
+TcpListener listener = new TcpListener(ipAddress, port);
+Dictionary<int, TcpClient> clients = new Dictionary<int, TcpClient>();
+// Dictionary<int, NetworkStream> streams = new Dictionary<int, NetworkStream>();
+// int nextClientId = 1; 
+NetworkStream clientStream = null;
 
-var listener = new TcpListener(ip, port);
+bool player1Turn = true;  
+bool gameEnded = false;
+
 listener.Start();
+
+BinaryWriter bw;
+BinaryReader br;
 
 while (true)
 {
-    var client = listener.AcceptTcpClient();
+
+    try
+    {
+        var client = listener.AcceptTcpClient();
+        clientStream = client.GetStream();
+        br = new BinaryReader(clientStream);
+        bw = new BinaryWriter(clientStream);
+
+        bw.Write(playerId++);
+
+        Console.WriteLine(playerId);
+
+        while (true)
+        {
+            client = listener.AcceptTcpClient();
+            Console.WriteLine("Connected to client.");
+
+            InitializeBoard();
+            PlayGame();
+
+        }
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    finally
+    {
+        if (clientStream != null)
+        {
+            clientStream.Close();
+        }
+        if (clientStream != null)
+        {
+            clientStream.Close();
+        }
+    }
+}
+
+void InitializeBoard()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            board[i, j] = ' ';
+        }
+    }
+}
 
 
-    var clientStream = client.GetStream();
-    var br = new BinaryReader(clientStream);
-    var bw = new BinaryWriter(clientStream);
+void PlayGame()
+{
+    while (!gameEnded)
+    {
+        SendGameBoard();
 
-    bw.Write(playerId++);
+        int[] move = GetMove();
 
-    Console.WriteLine(playerId);
-    
+        board[move[0], move[1]] = player1Turn ? 'X' : 'O';
+
+        if (CheckForWinner())
+        {
+            EndGame("Player " + (player1Turn ? "1" : "2") + " wins!");
+        }
+        else if (CheckForTie())
+        {
+            EndGame("The game is a tie.");
+        }
+
+        player1Turn = !player1Turn;
+    }
+}
+
+void SendGameBoard()
+{
+    string gameBoard = board[0, 0] + "|" + board[0, 1] + "|" + board[0, 2] + "|" +
+                       board[1, 0] + "|" + board[1, 1] + "|" + board[1, 2] + "|" +
+                       board[2, 0] + "|" + board[2, 1] + "|" + board[2, 2];
+    byte[] bytes = Encoding.ASCII.GetBytes(gameBoard);
+    bw.Write(bytes, 0, bytes.Length);
+}
+
+int[] GetMove()
+{
+    byte[] buffer = new byte[1024];
+    int bytesRead = br.Read(buffer, 0, buffer.Length);
+    string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+    int[] move = Array.ConvertAll(data.Split(','), int.Parse);
+    return move;
+}
+
+bool CheckForTie()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (board[i, j] == ' ')
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool CheckForWinner()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2] && board[i, 0] != ' ')
+        {
+            return true;
+        }
+
+        if (board[0, i] == board[1, i] && board[1, i] == board[2, i] && board[0, i] != ' ')
+        {
+            return true;
+        }
+    }
+
+    if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] && board[0, 0] != ' ')
+    {
+        return true;
+    }
+    if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0] && board[0, 2] != ' ')
+    {
+        return true;
+    }
+
+    return false;
+}
+
+void EndGame(string message)
+{
+    gameEnded = true;
+    byte[] bytes = Encoding.ASCII.GetBytes(message);
+    bw.Write(bytes, 0, bytes.Length);
 }
